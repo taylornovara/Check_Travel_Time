0d532568346a0532db911ee9cde8b1cc478307aa
taylornovara <taylornovara@gmail.com>
2022-09-24 07:50:42 -0500

    Created a scheduler that calls check_travel_time() every 30 minutes.

--- main.py
+++ main.py
@@ -10,63 +10,68 @@ import requests
 from twilio.rest import Client
 from datetime import datetime
 
-# API key
-with open("api_key.txt", "r") as api_file:
-    api_key = api_file.read()
 
-# Current time
-current_time = datetime.now().time()
-print(current_time)
-# Sets origin and destination based on time of day
-if current_time.hour < 12:
-    # Origin
-    start_name = "Lakeview Green"
-    start_location = "2901+4th+Ave+S+Birmingham+AL+35233"
+def check_travel_time():
+    # API key
+    with open("api_key.txt", "r") as api_file:
+        api_key = api_file.read()
 
-    # Destination
-    end_name = "Grandview"
-    end_location = "3690+Grandview+Pkwy+Birmingham+AL+35243"
-else:
-    # Origin
-    start_name = "Grandview"
-    start_location = "3690+Grandview+Pkwy+Birmingham+AL+35243"
+    # Current time
+    current_time = datetime.now().time()
+    print(current_time)
+    # Sets origin and destination based on time of day
+    if current_time.hour < 12:
+        # Origin
+        start_name = "Lakeview Green"
+        start_location = "2901+4th+Ave+S+Birmingham+AL+35233"
 
-    # Destination
-    end_name = "Lakeview Green"
-    end_location = "2901+4th+Ave+S+Birmingham+AL+35233"
+        # Destination
+        end_name = "Grandview"
+        end_location = "3690+Grandview+Pkwy+Birmingham+AL+35243"
+    else:
+        # Origin
+        start_name = "Grandview"
+        start_location = "3690+Grandview+Pkwy+Birmingham+AL+35243"
 
-# Base URL
-url = "https://maps.googleapis.com/maps/api/distancematrix/json"
+        # Destination
+        end_name = "Lakeview Green"
+        end_location = "2901+4th+Ave+S+Birmingham+AL+35233"
 
-# GET request
-r = requests.get(url + "?destinations=" + end_location + "&origins=" + start_location + "&departure_time=now" +
-                 "&key=" + api_key)
+    # Base URL
+    url = "https://maps.googleapis.com/maps/api/distancematrix/json"
 
-# Return time as text
-time = r.json()["rows"][0]["elements"][0]["duration_in_traffic"]["text"]
+    # GET request
+    r = requests.get(url + "?destinations=" + end_location + "&origins=" + start_location + "&departure_time=now" +
+                     "&key=" + api_key)
 
-# Print the total travel time
-travel_time = f"The total travel time from {start_name} to {end_name} is {time}."
-print(travel_time)
+    # Return time as text
+    time = r.json()["rows"][0]["elements"][0]["duration_in_traffic"]["text"]
 
-# Twilio account sid and authorization token
-with open("account_sid.txt", "r") as account_file:
-    account_sid = account_file.read()
-with open("auth_token.txt", "r") as auth_file:
-    auth_token = auth_file.read()
+    # Print the total travel time
+    travel_time = f"The total travel time from {start_name} to {end_name} is {time}."
+    print(travel_time)
 
-# Creates a Client object from Twilio
-client = Client(account_sid, auth_token)
+    # Twilio account sid and authorization token
+    with open("account_sid.txt", "r") as account_file:
+        account_sid = account_file.read()
+    with open("auth_token.txt", "r") as auth_file:
+        auth_token = auth_file.read()
 
-# List of numbers to text
-numbers_to_message = ['+12055639451', '+2053965212']
-# Looping through the numbers and send the text to each number
-for number in numbers_to_message:
-    message = client.messages.create(
-        messaging_service_sid='MG624d8354f0ca7aa37be60d1a39ed47de',
-        body=travel_time,
-        to=number
-    )
+    # Creates a Client object from Twilio
+    client = Client(account_sid, auth_token)
+
+    # List of numbers to text
+    numbers_to_message = ['+12055639451', '+12053965212']
+    # Looping through the numbers and send the text to each number
+    for number in numbers_to_message:
+        message = client.messages.create(
+            messaging_service_sid='MG624d8354f0ca7aa37be60d1a39ed47de',
+            body=travel_time,
+            to=number
+        )
 
 
-
+check_travel_time()
+scheduler = BlockingScheduler()
+scheduler.add_job(check_travel_time, 'interval', minutes=30)
+scheduler.start()
